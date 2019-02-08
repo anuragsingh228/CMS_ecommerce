186ea6b2692fddb58f7e9e319b83c74d38ac709c
Anurag Singh <anurags228@gmail.com>
2019-02-08 16:17:26 +0530

    basic routing

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var path = require("path");
 var mongoose = require('mongoose');
 var config = require('./config/database');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
+var expressValidator = require('express-validator');
 
 
 //connect to db
@@ -28,6 +29,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//express session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
+
+//express validator middleware
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.')
+            , root = namespace.shift()
+            , formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    },
+}));
+
+//express messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
 
 //set routes
 var pages = require('./routes/pages.js');
