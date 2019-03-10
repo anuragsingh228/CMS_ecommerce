c9b4fae9206435aa86d768a6c43261e071b8f116
Anurag Singh <anurags228@gmail.com>
2019-03-11 00:04:50 +0530

    admin rote set up

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+
+/* 
+*GET pages index
+*/
 router.get('/', function (req, res) {
     res.send('Admin area');
 });
 
+/* 
+*GET pages index
+*/
+router.get('/add-page', function (req, res) {
+    var title = "";
+    var slug = "";
+    var content = "";
+
+    res.render('admin/add_page', {
+        title: title,
+        slug: slug,
+        content: content
+    });
+
+});
 
 
 //Exports
