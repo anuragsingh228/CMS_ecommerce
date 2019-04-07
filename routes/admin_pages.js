dccdec3c3b13cf595717776b1e69e32c8c52f203
Anurag Singh <anurags228@gmail.com>
2019-04-07 11:32:04 +0530

    page file added

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+//Get Page model
+var Page = require('../models/page')
 
 /* 
 *GET pages index
 */
 router.get('/', function (req, res) {
-    res.send('Admin area');
+    Page.find({}).sort({ sorting: 1 }).exec(function (err, pages) {
+        res.render('admin/pages', {
+            pages: pages
+        });
+    });
 });
 
 /* 
@@ -49,7 +55,29 @@ router.post('/add-page', function (req, res) {
         });
     }
     else {
-        console.log('success');
+        Page.findOne({ slug: slug }, function (err, page) {
+            if (page) {
+                req.flash('danger', 'Page slug exists, choose another.');
+                res.render('admin/add_page', {
+                    title: title,
+                    slug: slug,
+                    content: content
+                });
+            } else {
+                var page = new Page({
+                    title: title,
+                    slug: slug,
+                    content: content,
+                    sorting: 100
+                });
+
+                page.save(function (err) {
+                    if (err) return console.log(err);
+                    req.flash('success', 'Page added');
+                    res.redirect('/admin/pages');
+                });
+            }
+        });
     }
 
 });
